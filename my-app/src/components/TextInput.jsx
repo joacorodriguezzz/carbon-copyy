db2258d8d43f2c7ea4a49ee1ffd42a92af13ed51
joacorodriguezzz <joaquinrodriguezz225@gmail.com>
2024-03-12 12:23:50 -0300

    Push 12/03

--- my-app/src/components/TextInput.jsx
+++ my-app/src/components/TextInput.jsx
@@ -1,24 +1,43 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import { MdOutlineContentCopy } from "react-icons/md";
 import { PiExportBold } from "react-icons/pi";
 import { FiTwitter } from "react-icons/fi";
 import { IoSettingsOutline } from "react-icons/io5";
 import { MdOutlineColorLens } from "react-icons/md";
+import { ChromePicker } from "react-color";
+import CodeEditor from "@uiw/react-textarea-code-editor";
+import Highlighter from "./common/CodeHighlighter";
+// import htmlToImage from "html-to-image";
 
 function App() {
+  const [code, setCode] = useState(`function add(a, b) {\n  return a + b;\n}`);
+  const [backgroundColor, setBackgroundColor] = useState("#555"); // Estado para el color de fondo del subcontenedor
+  const [displayColorPicker, setDisplayColorPicker] = useState(false); // Estado para mostrar/ocultar el color picker
+  const [inputValue, setInputValue] = useState(""); // Estado para el valor del input
+  const [copied, setCopied] = useState(false); // Estado para controlar si se ha copiado el contenido
+
+  const containerRef = useRef(null); // Referencia al contenedor principal
+
   const containerStyle = {
     display: "flex",
     justifyContent: "center",
     alignItems: "center",
-    height: "73vh",
+    minHeight: "100vh",
     backgroundColor: "#212529",
   };
 
-  const formularioStyle = {
-    width: "907.36px",
-    height: "479px",
+  const subContainerStyle = {
+    backgroundColor: "#333", // Usando el color de fondo del estado
+    width: "800px",
     padding: "20px",
-    backgroundColor: "#333",
+    borderRadius: "10px",
+  };
+
+  const formularioStyle = {
+    maxWidth: "200%",
+    width: "auto",
+    padding: "20px",
+    backgroundColor: backgroundColor,
     borderRadius: "10px",
     boxShadow: "0 0 10px rgba(0, 0, 0, 0.5)",
     textAlign: "center",
@@ -26,8 +45,8 @@ function App() {
   };
 
   const inputStyle = {
-    width: "587px",
-    height: "360px",
+    width: "100%",
+    height: "300px",
     marginBottom: "10px",
     padding: "5px",
     borderRadius: "5px",
@@ -41,17 +60,18 @@ function App() {
   const navBarStyle = {
     display: "flex",
     justifyContent: "center",
+    flexWrap: "wrap",
     marginBottom: "10px",
   };
 
   const selectStyle = {
     marginRight: "10px",
+    marginBottom: "10px",
     padding: "5px",
     borderRadius: "5px",
     border: "none",
     backgroundColor: "#444",
     color: "#fff",
-    width: "100px",
   };
 
   const buttonStyle = {
@@ -65,54 +85,120 @@ function App() {
     cursor: "pointer",
     transition: "background-color 0.3s",
   };
-  buttonStyle[":hover"] = {
-    backgroundColor: "#888",
+
+  const handleColorChange = (color) => {
+    setBackgroundColor(color.hex); // Actualizar el color de fondo del subcontenedor
   };
+
+  const handleClick = () => {
+    setDisplayColorPicker(!displayColorPicker); // Mostrar u ocultar el color picker
+  };
+
+  const handleClose = () => {
+    setDisplayColorPicker(false); // Ocultar el color picker cuando se cierra
+  };
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value); // Actualizar el valor del input
+  };
+
+  const handleCopyClick = () => {
+    navigator.clipboard.writeText(inputValue); // Copiar el valor del input al portapapeles
+    setCopied(true); // Establecer el estado de copiado a verdadero
+    setTimeout(() => {
+      setCopied(false); // Restablecer el estado de copiado después de 2 segundos
+    }, 2000);
+  };
+
+  // const handleExportClick = () => {
+  //   htmlToImage
+  //     .toPng(containerRef.current)
+  //     .then(function (dataUrl) {
+  //       var link = document.createElement("a");
+  //       link.download = "snippet.png";
+  //       link.href = dataUrl;
+  //       link.click();
+  //     })
+  //     .catch(function (error) {
+  //       console.error("Error exporting snippet:", error);
+  //     });
+  // };
 
   return (
     <div style={containerStyle}>
-      <div style={formularioStyle}>
-        <div style={navBarStyle}>
-          <select style={selectStyle}>
-            <option value="" hidden>
-              Theme
-            </option>
-            <option value="opcion1">Opción 1</option>
-            <option value="opcion2">Opción 2</option>
-            <option value="opcion3">Opción 3</option>
-          </select>
-          <select style={selectStyle}>
-            <option value="" hidden>
-              Language
-            </option>
-            <option value="opcion1">JS</option>
-            <option value="opcion3">JSON</option>
-            <option value="opcion2">JSX</option>
-            <option value="opcion3">HTML</option>
-          </select>
+      <div style={subContainerStyle} ref={containerRef}>
+        <div style={formularioStyle}>
           <div style={navBarStyle}>
-            <button style={buttonStyle} disabled>
-              <IoSettingsOutline />
-            </button>
-            <button style={buttonStyle} disabled>
-              <MdOutlineContentCopy />
-            </button>
-            <button style={buttonStyle} disabled>
-              <FiTwitter />
-            </button>
-            <button style={buttonStyle}>
-              <PiExportBold />
-            </button>
+            <select style={selectStyle}>
+              <option value="" hidden>
+                Theme
+              </option>
+              <option value="opcion1">Opción 1</option>
+              <option value="opcion2">Opción 2</option>
+              <option value="opcion3">Opción 3</option>
+            </select>
+            <select style={selectStyle}>
+              <option value="" hidden>
+                Language
+              </option>
+              <option value="opcion1">JS</option>
+              <option value="opcion3">JSON</option>
+              <option value="opcion2">JSX</option>
+              <option value="opcion3">HTML</option>
+            </select>
+            <div style={navBarStyle}>
+              <button
+                style={buttonStyle}
+                onClick={handleCopyClick}
+                className={copied ? "copied" : ""}
+              >
+                <MdOutlineContentCopy />
+              </button>
+              <button style={buttonStyle}>
+                <PiExportBold />
+              </button>
+              <div style={{ position: "relative" }}>
+                <button style={buttonStyle} onClick={handleClick}>
+                  <MdOutlineColorLens />
+                </button>
+                {displayColorPicker && (
+                  <div style={{ position: "absolute", zIndex: "2" }}>
+                    <div
+                      style={{
+                        position: "fixed",
+                        top: "0px",
+                        right: "0px",
+                        bottom: "0px",
+                        left: "0px",
+                      }}
+                      onClick={handleClose}
+                    />
+                    <ChromePicker
+                      color={backgroundColor}
+                      onChange={handleColorChange}
+                    />
+                  </div>
+                )}
+              </div>
+            </div>
           </div>
+          <CodeEditor
+            value={code}
+            language="html"
+            placeholder="Please enter JS code."
+            onChange={(evn) => setCode(evn.target.value)}
+            padding={15}
+            style={{
+              backgroundColor: "#f5f5f5",
+              fontFamily:
+                "ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace",
+            }}
+          />
         </div>
-        <input
-          type="text"
-          placeholder="Type your code here"
-          style={inputStyle}
-        />
       </div>
     </div>
   );
 }
 
 export default App;
+// onClick={handleExportClick}
