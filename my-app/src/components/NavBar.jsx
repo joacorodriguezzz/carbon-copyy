bc624e2d7907972c5f59d494bc0e17680822a0c9
NACHO-ASUS-PC\nrodr <ignacio.rodriguez.r@outsourcing.cepsa.com>
2024-03-14 15:03:20 -0300

    darkmode backtips

--- my-app/src/components/NavBar.jsx
+++ my-app/src/components/NavBar.jsx
@@ -1,39 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import { setUser } from '../state/user';
+import { MdInvertColors } from "react-icons/md";
+
+
 const NavBar = ({ user }) => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setIsDarkMode(!isDarkMode);
+  };
+
   return (
-    // <>
-    //   {user.email ? (
-        <Navbar expand="lg" className="bg-light text-light">
-          <Container>
-            <Navbar.Brand href="/">Carbon Copy</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="me-auto">
-                <Nav.Link href="/login">Login</Nav.Link>
-                <Nav.Link href="/register">SignUp</Nav.Link>
-                <Nav.Link href="/favoritesList">Favoritos</Nav.Link>
-              </Nav>
-            </Navbar.Collapse>
-          </Container>
-        </Navbar>
-    //   ) : (
-    //     <Navbar expand="lg" className="bg-light text-light">
-    //       <Container>
-    //         <Navbar.Brand href="/">Carbon Copy</Navbar.Brand>
-    //         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    //         <Navbar.Collapse id="basic-navbar-nav">
-    //           <Nav className="me-auto">
-    //             <Nav.Link href="/login">Login</Nav.Link>
-    //             <Nav.Link href="/register">SignUp</Nav.Link>
-    //           </Nav>
-    //         </Navbar.Collapse>
-    //       </Container>
-    //     </Navbar>
-    //   )}
-    // </>
+    <div className={`Navbar ${isDarkMode ? 'dark-mode' : 'light-mode'}`}>
+      <Navbar expand="lg" className={`bg-${isDarkMode ? 'dark' : 'light'} text-${isDarkMode ? 'light' : 'dark'}`}>
+        <Container>
+          <Navbar.Brand href="/">Carbon Copy</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="/login">Login</Nav.Link>
+              <Nav.Link href="/register">SignUp</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+          <button onClick={toggleDarkMode} className="btn btn-sm btn-primary"><MdInvertColors />
+</button>
+        </Container>
+      </Navbar>
+    </div>
   );
 };
 
-export default NavBar;
+export default NavBar;
